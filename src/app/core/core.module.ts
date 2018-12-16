927711c58b313b733422887f20f3539fadb77469
zyx <1113470466@qq.com>
2018-12-16 13:54:26 +0800

    initial commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class CoreM
