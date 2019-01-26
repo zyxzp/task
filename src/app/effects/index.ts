70483645162727d5d66d8661eb121c92f8a3c510
zyxzp <1113470466@qq.com>
2019-01-26 14:16:52 +0800

    Update index.ts

--- src/app/effects/index.ts
+++ src/app/effects/index.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { QuoteEffects } from './quote.effects';
-
+import { AuthEffects } from './auth.effects';
 
 @NgModule({
     imports: [
-        EffectsModule.forRoot([QuoteEffects])
+        EffectsModule.forRoot([QuoteEffects, AuthEffects])
     ],
 
 })
