d914fc980cdd9a669179151b03b2303b9859f9f3
zyxzp <1113470466@qq.com>
2019-01-26 14:23:14 +0800

    Update auth-guard.service.ts

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../reducers';
+import { map, defaultIfEmpty } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
+    constructor(private store$: Store<fromRoot.State>,
+        private router: Router) {
+
+    }
     canActivate(
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
-    ): Observable<boolean>{
-        return of(true);
+    ): Observable<boolean> {
+        // this.store$.pipe(select(fromRoot.getAuthState),
+        //     map(auth => {
+        //         const result = auth.token != null && auth.token != undefined;
+        //         if (result) {
+        //             this.router.navigate(['/login']);
+        //         }
+        //         return of(result);
+        //     }),
+        //     defaultIfEmpty(of(false))
+        //     )
+        return of(false);
     }
 }
+
