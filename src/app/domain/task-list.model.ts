aefd76cac2c13bc21944380d60f8cc0b0e4a9dba
zyxzp <1113470466@qq.com>
2019-01-28 15:03:26 +0800

    Update task-list.model.ts

--- src/app/domain/task-list.model.ts
+++ src/app/domain/task-list.model.ts
@@ -1,8 +1,10 @@
+import { Task } from './task.model';
+
 export interface TaskList {
     id?: string;
     name: string;
     projectId: string;
     order: number;
     taskIds?: string[];
+    tasks?: Task[];
   }
-  
