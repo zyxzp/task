dacdfec3a16abb25a82ce144f83f14dfa36f1586
zyxzp <1113470466@qq.com>
2019-01-28 15:09:45 +0800

    Update task-home.component.ts

--- src/app/task/task-home/task-home.component.ts
+++ src/app/task/task-home/task-home.component.ts
@@ -8,11 +8,12 @@ import { slideToRight } from '../../anims/router.anim';
 import { Store, select } from '@ngrx/store';
 import * as fromRoot from '../../reducers';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { pluck, take, filter, map, switchMap } from 'rxjs/operators';
 import * as taskListActions from '../../actions/task-list.actions';
 import * as taskActions from '../../actions/task.actions';
-import { Task } from 'src/app/domain';
+import { TaskList, Task } from 'src/app/domain';
+
 @Component({
   selector: 'app-task-home',
   templateUrl: './task-home.component.html',
@@ -30,7 +31,7 @@ export class TaskHomeComponent implements OnInit {
     private store: Store<fromRoot.State>,
     private route: ActivatedRoute) {
     this.projectId$ = this.route.paramMap.pipe(pluck('id'));
-    this.lists$ = this.store.pipe(select(fromRoot.getTasksByLists))
+    this.lists$ = this.store.pipe(select(fromRoot.getTasksByLists));
   }
 
   ngOnInit() {
@@ -45,7 +46,7 @@ export class TaskHomeComponent implements OnInit {
         take(1),
         filter(n => n)
       ))
-    ).subscribe((task) => {
+    ).subscribe((task: Task) => {
       this.store.dispatch(new taskActions.AddAction(
         {
           ...task,
@@ -65,69 +66,68 @@ export class TaskHomeComponent implements OnInit {
       ))
     ).subscribe((val: string) => {
       this.store.dispatch(new taskActions.MoveAllAction({ srcListId: list.id, targetListId: val }))
-  });
+    });
 
-}
-launchUpdateTaskDialog(task) {
-  const dialogRef = this.dialog.open(NewTaskComponent, { data: { title: '修改任务', task: task } });
-  dialogRef.afterClosed().pipe(
-    take(1),
-    filter(n => n)
-  ).subscribe(val => {
-    this.store.dispatch(new taskActions.UpdateAction({ ...task, ...val }));
-  });
-
-}
-
-//列表相关操作
-//新建任务列表
-openNewListDialog(ev: Event) {
-  const dialogRef = this.dialog.open(NewTaskListComponent, { data: { title: '新建列表' } });
-  dialogRef.afterClosed().pipe(
-    take(1)
-  ).subscribe(res => {
-    this.store.dispatch(new taskListActions.AddAction(res));
-  });
-}
-launchEditList(list: TaskList) {
-  const dialogRef = this.dialog.open(NewTaskListComponent, { data: { title: '修改列表', taskList: list } });
-  dialogRef.afterClosed().pipe(
-    take(1)
-  ).subscribe(res => {
-    this.store.dispatch(new taskListActions.UpdateAction({ ...res, id: list.id }));
-  });
-}
-
-launchDeleteList(list: TaskList) {
-  const dialogRef = this.dialog.open(ConfirmDialogComponent, { data: { title: '删除列表', content: '确认删除选中列表吗?' } });
-  dialogRef.afterClosed().pipe(
-    take(1),
-    filter(n => n)
-  ).subscribe(res => {
-    this.store.dispatch(new taskListActions.DeleteAction(list));
-  });
-}
-
-handleMove(srcData, list: TaskList) {
-  switch (srcData.tag) {
-    case 'task-item': {
-      break;
-    }
-    case 'task-list': {
-      const order = srcData.data.order;
-      srcData.data.order = list.order;
-      list.order = order;
-      break;
-    }
-    default:
-      break;
   }
-}
-}
-export interface TaskList {
-  id?: string;
-  name: string;
-  projectId: string;
-  order: number;
-  taskIds?: string[];
+  launchUpdateTaskDialog(task) {
+    const dialogRef = this.dialog.open(NewTaskComponent, { data: { title: '修改任务', task: task } });
+    dialogRef.afterClosed().pipe(
+      take(1)
+    ).subscribe(res => {
+      this.store.dispatch(new taskActions.UpdateAction({ ...res, id: task.id }))
+    });
+
+
+  }
+  getTaskByIds(taskIds) {
+    return this.store.pipe(select(fromRoot.getTaskByIds(taskIds)));
+  }
+  //列表相关操作
+  //新建任务列表
+  openNewListDialog(ev: Event) {
+    const dialogRef = this.dialog.open(NewTaskListComponent, { data: { title: '新建列表' } });
+    dialogRef.afterClosed().pipe(
+      take(1)
+    ).subscribe(res => {
+      this.store.dispatch(new taskListActions.AddAction({
+        ...res,
+        projectId: this.projectId$.pipe(map(val => val))
+      }));
+    });
+  }
+  launchEditList(list: TaskList) {
+    const dialogRef = this.dialog.open(NewTaskListComponent, { data: { title: '修改列表', taskList: list } });
+    dialogRef.afterClosed().pipe(
+      take(1)
+    ).subscribe(res => {
+      this.store.dispatch(new taskListActions.UpdateAction({ ...res, id: list.id }));
+    });
+  }
+
+  launchDeleteList(list: TaskList) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, { data: { title: '删除列表', content: '确认删除选中列表吗?' } });
+    dialogRef.afterClosed().pipe(
+      take(1),
+      filter(n => n)
+    ).subscribe(res => {
+      this.store.dispatch(new taskListActions.DeleteAction(list));
+    });
+  }
+
+  handleMove(srcData, list: TaskList) {
+    switch (srcData.tag) {
+      case 'task-item': {
+        this.store.dispatch(new taskActions.MoveAction(
+          { taskId: srcData.data.id, tasklistId: list.id }
+        ));
+        break;
+      }
+      case 'task-list': {
+        this.store.dispatch(new taskListActions.SwapAction({ src: srcData.data, target: list }));
+        break;
+      }
+      default:
+        break;
+    }
+  }
 }
