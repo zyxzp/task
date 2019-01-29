d50d167e6565d56a29019c543655910e58856ed2
zyxzp <1113470466@qq.com>
2019-01-29 15:18:41 +0800

    Update app.component.spec.ts
    app.component 单元测试用例

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,47 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
+import { CoreModule } from './core/core.module';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material';
+import { APP_BASE_HREF } from '@angular/common';
+import { AppStoreModule } from './reducers';
+import { CounterComponent } from './counter/counter.component';
+import { AppEffectsModule } from './effects';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        MatSidenavModule,
+        RouterModule.forRoot([]),
+        CoreModule,
+        AppStoreModule,
+        AppEffectsModule,
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        CounterComponent
       ],
+      providers:[{
+        provide:APP_BASE_HREF,
+        useValue:'/'
+      }]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('应该创建用例', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'task'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('task');
-  });
 
-  it('should render title in a h1 tag', () => {
+
+  it('应该包含一个 .site 的元素', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to task!');
+    expect(compiled.querySelector('.site')).toBeTruthy();
   });
 });
